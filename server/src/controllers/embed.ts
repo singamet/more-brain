68b4ea6dfa43b882b41c8467d21dba4cace20349
singamet <ametojsingh007@gmail.com>
2025-04-09 01:32:34 +0530

    Embed headers

--- server/src/controllers/embed.ts
+++ server/src/controllers/embed.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 
 export const getRedditPost = async (req: Request, res: Response) => {
   const { url } = req.query;
-
-  const response = await fetch(`https://www.reddit.com/oembed?url=${url}`, {
-    headers: {
-      "User-Agent": "More Brain/1.0 (singamet@outlook.com)",
-    },
-  });
+  const encodedUrl = encodeURIComponent(url as string);
+  const response = await fetch(
+    `https://www.reddit.com/oembed?url=${encodedUrl}`,
+    {
+      headers: {
+        "User-Agent": "More Brain/1.0 (singamet@outlook.com)",
+      },
+    }
+  );
   const data = await response.json();
   if (!response.ok) {
     res.status(404).json({ error: data });
